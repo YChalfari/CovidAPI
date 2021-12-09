4d147611420a256ea4e8b441a0f10382affc7ceb
Yitz Halfari <chalfari@gmail.comgit config --global user.email chalfari@gmail.com>
2021-12-09 12:54:59 +0200

    added pseudo code, pdf, and html w links

---
+++ scripts/pseudo.js
@@ -0,0 +1,13 @@
+//! Pseudo Code
+const continents = {
+  asia: {},
+  europe: {},
+  america: {},
+  asia: {},
+  asia: {},
+  update: updateContinents,
+};
+const main = () => {};
+function updateContinents(country) {}
+function isPending(params) {}
+function disableButton(params) {}
