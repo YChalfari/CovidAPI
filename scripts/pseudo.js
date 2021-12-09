6eafca0bd67d1c2a599e35af4007d6252738c1bd
Yitz Halfari <chalfari@gmail.comgit config --global user.email chalfari@gmail.com>
2021-12-09 16:36:24 +0200

    added base CSS layout and chart npm

--- scripts/pseudo.js
+++ scripts/pseudo.js
@@ -7,7 +7,11 @@ const continents = {
   asia: {},
   update: updateContinents,
 };
+const buttonState = {};
 const main = () => {};
 function updateContinents(country) {}
 function isPending(params) {}
 function disableButton(params) {}
+function calcPercentage() {}
+function showPercentage(params) {}
+function drawGraph(params) {}
---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+const continentGraph = document.querySelector();
+const continentSelect = document.querySelector();
+const countrySelect = document.querySelector();
+const countryGraph = document.querySelector();
