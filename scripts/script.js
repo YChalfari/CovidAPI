92959f1630b8b3c70d11d53f45607a7699651656
Yitz Halfari <chalfari@gmail.comgit config --global user.email chalfari@gmail.com>
2021-12-09 21:36:46 +0200

    added function for subregion and error function

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,86 @@
 import { continentGraph, contChartObj } from "./chart.js";
 const PROXY_URL = "https://intense-mesa-62220.herokuapp.com/";
-const CONT_LINK = "https://restcountries.herokuapp.com/api/v1/region/";
+const CONT_URL = "https://restcountries.herokuapp.com/api/v1/region/";
+const REGION_URL = "https://restcountries.herokuapp.com/api/v1/subregion/";
+const COVID_URL = "http://corona-api.com/countries/";
 // const continentSelect = document.querySelector();
 // const countrySelect = document.querySelector();
 // const countryGraph = document.querySelector();
+const errorDiv = document.querySelector(".error-container");
 const continents = {
   asia: {},
   europe: {},
   americas: {},
   oceania: {},
-  asia: {},
+  antarctica: {},
 };
+const countryCodes = [];
 const buttonState = {};
 const main = () => {};
 
-async function fetchCountries(continent) {
-  const countriesJson = await fetch(`${PROXY_URL}${CONT_LINK}${continent}`);
-  const parsedCountries = await countriesJson.json();
-  console.log(parsedCountries);
-  saveData(continent, parsedCountries);
+async function fetchCountriesInCont(continent) {
+  try {
+    const countriesJson = await fetch(`${PROXY_URL}${CONT_URL}${continent}`);
+    if (!countriesJson.ok) {
+      throw Error(`${countriesJson.status}`);
+    }
+    const parsedCountries = await countriesJson.json();
+
+    saveCodes(continent, parsedCountries);
+    fetchCountry(countryCodes);
+  } catch (e) {
+    errorMessage(`Oops, Something went wrong. Server Message: ${e.message}`);
+  }
 }
-fetchCountries("oceania");
+// fetchCountriesInCont("antarctica");
+async function fetchCountriesInSubRegion(subregion) {
+  try {
+    const regionJson = await fetch(`${PROXY_URL}${REGION_URL}${subregion}`);
+    if (!regionJson.ok) {
+      throw Error(`${regionJson.status}`);
+    }
+    const parsedCountries = await regionJson.json();
+    console.log(parsedCountries);
+    saveCodes(subregion, parsedCountries);
+    fetchCountry(countryCodes);
+  } catch (e) {
+    errorMessage(`Oops, Something went wrong. Server Message: ${e.message}`);
+  }
+}
+// fetchCountriesInSubRegion("northern america");
 
-async function fetchCountry(codes) {}
-
-function saveData(continent, countries) {
-  countries.forEach((country) => {
-    const { name: countryName, cca2 } = country;
-    continents[continent][countryName.common] = { code: cca2 };
-    console.log(continents);
+async function fetchCountry(codes) {
+  codes.forEach(async (code) => {
+    try {
+      const covidJson = await fetch(`${PROXY_URL}${COVID_URL}${code}`);
+      if (!covidJson.ok) {
+        throw Error(`${covidJson.status}`);
+      }
+      const parsedCountries = await covidJson.json();
+      console.log(parsedCountries);
+    } catch (e) {
+      errorMessage(`Oops, Something went wrong. Server Message: ${e.message}`);
+    }
   });
 }
+
+function saveCodes(continent, countries) {
+  countries.forEach((country) => {
+    const { cca2 } = country;
+    countryCodes.push(cca2);
+  });
+}
+function saveCountryData() {}
+function errorMessage(e) {
+  errorDiv.textContent = e;
+}
+// function saveData(continent, countries) {
+//   countries.forEach((country) => {
+//     const { name: countryName, cca2 } = country;
+//     continents[continent][countryName.common] = { code: cca2 };
+//     console.log(continents);
+//   });
+// }
 function updateChart(countriesObj) {}
 function isPending(params) {}
 function disableButton(params) {}
